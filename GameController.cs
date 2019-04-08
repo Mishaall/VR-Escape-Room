db0c979a1680d72bdc2e00543e2a70f61e4e3555
Mishaall <45889290+Mishaall@users.noreply.github.com>
2019-04-08 17:55:35 -0400

    Add files via upload

---
+++ GameController.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour {
+
+	public Player player;
+	public GameObject door;
+	public GameObject itemContainer;
+	public TextMesh gameText;
+
+	private Vector3 originalDoorPosition;
+	private float gameTimer;
+	private float gameOverTimer = 3f;
+
+	// Use this for initialization
+	void Start () {
+		originalDoorPosition = door.transform.position;
+
+		for (int i = 0; i < 50; i++) {
+			GameObject item1 = itemContainer.transform.GetChild(Random.Range(0, itemContainer.transform.childCount)).gameObject;
+			GameObject item2 = itemContainer.transform.GetChild(Random.Range(0, itemContainer.transform.childCount)).gameObject;
+
+			Vector3 item1position = item1.transform.position;
+			item1.transform.position = item2.transform.position;
+			item2.transform.position = item1position;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (player.hasKey) {
+			door.transform.position = Vector3.Lerp (
+				door.transform.position,
+				originalDoorPosition + new Vector3 (0, 10, 0),
+				Time.deltaTime
+			);
+
+			gameText.text = "You won :D\nYour time: " + Mathf.FloorToInt(gameTimer);
+
+			gameOverTimer -= Time.deltaTime;
+			if (gameOverTimer <= 0) {
+				SceneManager.LoadScene (SceneManager.GetActiveScene().name);
+			}
+		} else {
+			gameTimer += Time.deltaTime;
+
+			gameText.text = "Find the key!\nTime: " + Mathf.FloorToInt(gameTimer);
+		}
+	}
+}
---
+++ Player.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	public float movementSpeed = 2.5f;
+	public bool hasKey = false;
+
+	private Vector3 targetPosition;
+
+	// Use this for initialization
+	void Start () {
+		targetPosition = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		RaycastHit hit;
+
+		if (Physics.Raycast(transform.position, transform.forward, out hit)) {
+			if (GvrViewer.Instance.Triggered || Input.GetKeyDown("space")) {
+
+				if (hit.transform.tag == "Waypoint") {
+					targetPosition = new Vector3 (
+						hit.transform.position.x,
+						transform.position.y,
+						hit.transform.position.z
+					);
+				} else if (hit.transform.tag == "CorrectItem") {
+					hasKey = true;
+
+					Destroy (hit.transform.gameObject);
+				}
+
+			}
+		}
+
+		transform.position = Vector3.Lerp (transform.position, targetPosition, movementSpeed * Time.deltaTime);
+	}
+}
